6d4a75e796951c5fae88cb4f28aaee2f2409366e
Hans Chehaiber <39745948+hanschehaiber@users.noreply.github.com>
2023-03-17 23:45:18 -0400

    switched to using async matchers, created test step in before each

--- tests/sauce.demo.spec.ts
+++ tests/sauce.demo.spec.ts
@@ -1,12 +1,19 @@
+import { expect, test } from "@playwright/test";
+
 import { CheckoutCompletePage } from "./pages/checkout.complete.page";
 import { CheckoutOverviewPage } from "./pages/checkout.overview.page";
 import { CheckoutInfoPage } from "./pages/checkout.info.page";
 import { ShoppingCart } from "./pages/shopping.cart.page";
-import { expect, test } from "@playwright/test";
 import { LoginPage } from "./pages/login.page";
 import { ProductsPage } from "./pages/products.page";
 
 test.describe("Sauce Demo Happy Path Test Suite", async () => {
+  const username = "standard_user";
+  const password = "secret_sauce";
+  const firstName = "John";
+  const lastName = "Smith";
+  const postalCode = "12345";
+
   let productsPage: ProductsPage;
   let loginPage: LoginPage;
   let shoppingCart: ShoppingCart;
@@ -15,29 +22,24 @@ test.describe("Sauce Demo Happy Path Test Suite", async () => {
   let checkoutCompletePage: CheckoutCompletePage;
 
   test.beforeEach(async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
     loginPage = new LoginPage(page);
     productsPage = new ProductsPage(page);
     checkoutInfoPage = new CheckoutInfoPage(page);
     checkoutOverviewPage = new CheckoutOverviewPage(page);
     checkoutCompletePage = new CheckoutCompletePage(page);
     shoppingCart = new ShoppingCart(page);
+
+    test.step("Go to Login Page and Login", async () => {
+      await page.goto("https://www.saucedemo.com/");
+      await loginPage.login(username, password);
+    });
   });
 
   test("Add a single item to cart and checkout", async () => {
-    const username = "standard_user";
-    const password = "secret_sauce";
-    const firstName = "John";
-    const lastName = "Smith";
-    const postalCode = "12345";
-
-    await loginPage.login(username, password);
     const priceOnProductsPage = await productsPage.getFirstProductPrice();
     await productsPage.addFirstProductToCart();
     await productsPage.shoppingCart.click();
-    const numberOfItemsInCart = await shoppingCart.getNumberOfItemsInCart();
-    expect(numberOfItemsInCart).toBe(1);
-
+    await expect(shoppingCart.cartItems).toHaveCount(1);
     await shoppingCart.checkout.click();
 
     await checkoutInfoPage.fillOutAndSubmitCheckoutInfo(
@@ -46,13 +48,13 @@ test.describe("Sauce Demo Happy Path Test Suite", async () => {
       postalCode
     );
 
-    expect(await checkoutOverviewPage.getSubtotal()).toContain(
+    await expect(checkoutOverviewPage.subtotal).toContainText(
       priceOnProductsPage
     );
     await checkoutOverviewPage.finish.click();
     const expectedMessage =
       "Your order has been dispatched, and will arrive just as fast as the pony can get there!";
-    expect(await checkoutCompletePage.getCompleteText()).toEqual(
+    await expect(checkoutCompletePage.completeText).toContainText(
       expectedMessage
     );
   });
