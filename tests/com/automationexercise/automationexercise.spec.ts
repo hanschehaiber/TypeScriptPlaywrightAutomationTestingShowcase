97b297a349664dad95b140b328ca931d670e3aec
Hans Chehaiber <39745948+hanschehaiber@users.noreply.github.com>
2024-02-22 20:31:24 -0500

    Create a login test

--- tests/com/automationexercise/automationexercise.spec.ts
+++ tests/com/automationexercise/automationexercise.spec.ts
@@ -1,12 +1,21 @@
 import { expect, test } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-    await page.goto('https://www.automationexercise.com/login');
-})
+test.describe('Test Suite', async () => {
+    const email = 'JohnSmith@example.com'
+    const password = 'test1234';
 
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://www.automationexercise.com/login');
+    });
 
-
-test("Login Test", async ({ page }) => {
-    await page.waitForTimeout(5000);
-
-})
+    test.afterEach(async ({ page }) => {
+        await page.close();
+    });
+    
+    test("Login Test", async ({ page }) => {
+        await page.locator("[data-qa='login-password']").fill(password);
+        await page.locator("[data-qa='login-email']").fill(email);
+        await page.locator("[data-qa='login-button']").click();
+        await page.locator('li').filter({ hasText: 'Logged in as John Smith' }).isVisible();
+    });
+});
