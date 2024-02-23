d446476529511be9a6faaf2a461f57255c1623a6
Hans Chehaiber <39745948+hanschehaiber@users.noreply.github.com>
2024-02-23 14:34:26 -0500

    renamed locator

--- tests/com/saucedemo/pages/login.page.ts
+++ tests/com/saucedemo/pages/login.page.ts
@@ -4,18 +4,18 @@ import { BasePage } from "./base.page";
 export class LoginPage extends BasePage {
   readonly username: Locator;
   readonly password: Locator;
-  readonly submitLoginButton: Locator;
+  readonly loginButton: Locator;
 
   constructor(page: Page) {
     super(page);
     this.username = page.locator("[data-test='username']");
     this.password = page.locator("#password");
-    this.submitLoginButton = page.locator("#login-button");
+    this.loginButton = page.locator("#login-button");
   }
 
   async login(username: string, password: string) {
     await this.username.fill(username);
     await this.password.fill(password);
-    await this.submitLoginButton.click();
+    await this.loginButton.click();
   }
 }
